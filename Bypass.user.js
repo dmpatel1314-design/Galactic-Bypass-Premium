44dd6aa2da5220d38f2f62496094382694f5153d
dmpatel1314-design <dmpatel1314@gmail.com>
2025-12-09 17:10:20 -0800

    Add Galactic Bypass Userscript version 2.1.6

---
+++ Bypass.user.js
@@ -0,0 +1,105 @@
+// ==UserScript==
+// @name         Galactic Bypass Userscript [Premium]
+// @homepageURL  https://linkvertise-bypass.com
+// @namespace    https://tampermonkey.net/
+// @version      2.1.6
+// @description  Premium Bypasser
+// @author       RealYoGalactic
+// @match        *://linkvertise.com/*/*
+// @match        *://linkvertise.com/?hash=*
+// @match        *://loot-link.com/s?*
+// @match        *://loot-links.com/s?*
+// @match        *://lootlink.org/s?*
+// @match        *://lootlinks.co/s?*
+// @match        *://lootdest.info/s?*
+// @match        *://lootdest.org/s?*
+// @match        *://lootdest.com/s?*
+// @match        *://links-loot.com/s?*
+// @match        *://linksloot.net/s?*
+// @match        *://spdmteam.com/key-system*
+// @match        *://rekonise.com/*
+// @match        *://rekonise.org/*
+// @match        *://linkunlocker.com/*
+// @match        *://bstlar.com/*
+// @match        *://mboost.me/*
+// @match        *://go.linkify.ru/*
+// @match        *://blox-script.com/get-key*
+// @match        *://paster.so/*
+// @match        *://sub2unlock.io/*
+// @match        *://sub4unlock.pro/*
+// @match        *://sub4unlock.com/*
+// @match        *://sub2unlock.com/*
+// @match        *://sub2unlock.top/*
+// @match        *://sub2unlock.me/*
+// @match        *://*.sub2get.com/*
+// @match        *://dusarisalary.com/*
+// @match        *://bloggingdaze.com/*
+// @match        *://ldnesfspublic.org/*
+// @match        *://adfoc.us/*
+// @match        *://socialwolvez.com/*
+// @match        *://boost.ink/*
+// @match        *://social-unlock.com/*
+// @match        *://mobile.codex.lol/*
+// @match        *://auth.platoboost.com/*
+// @match        *://auth.platoboost.click/*
+// @match        *://auth.platoboost.net/*
+// @match        *://auth.platorelay.com/*
+// @match        *://auth.platoboost.app/*
+// @matcb        *://auth.platoboost.me/*
+// @match        *://keyrblx.com/*
+// @match        *://pandadevelopment.net/getkey?*
+// @match        *://link-unlock.com/*
+// @match        *://link2unlock.com/*
+// @match        *://archub.dev/key-system/getkey?hwid=*
+// @match        *://deltaios-executor.com/ads.html?URL=*
+// @match        *://key.volcano.wtf/*
+// @match        *://*.tapvietcode.com/*
+// @match        *://getkey.farrghii.com/*
+// @match        *://quartyz.dev/*
+// @match        *://quartyz.com/*
+// @match        *://bstshrt.com/*
+// @match        *://hehehub-acsu123.pythonanywhere.com/api/getkey?hwid=*
+// @match        *://key.thanhub.com/*
+// @match        *://lockr.so/thank-you*
+// @require      https://github.com/BaconButPro/ron12373/raw/Premium/Bypass-Userscript/style.js
+// @require      https://cdn.rawgit.com/ricmoo/aes-js/e27b99df/index.js
+// @require      https://api2.adbypass.org/userscript/download/bypass.meta.js
+// @grant        GM.xmlHttpRequest
+// @grant        GM_xmlhttpRequest
+// @grant        GM_setClipboard
+// @grant        GM_info
+// @grant        unsafeWindow
+// @run-at       document-end
+// @connect      linkvertise.com
+// @connect      api.codex.lol
+// @connect      keyrblx.com
+// @connect      pandadevelopment.net
+// @connect      loot-link.com
+// @connect      loot-links.com
+// @connect      lootlink.org
+// @connect      lootlinks.co
+// @connect      lootdest.info
+// @connect      lootdest.org
+// @connect      lootdest.com
+// @connect      links-loot.com
+// @connect      linksloot.net
+// @connect      auth.platorelay.com
+// @connect      auth.platoboost.app
+// @connect      auth.platoboost.net
+// @connect      auth.platoboost.me
+// @icon         https://linkvertise-bypass.com/favicon.ico
+// ==/UserScript==
+
+function key() {
+    return ''; // Your Key
+}
+
+function config() {
+    return {
+        auto_copy: 'true', // Auto Copy: 'true' or 'false'
+        AntiBlacklist_Linkvertise: true, // Anti Blacklist Linkvertise [Beta]
+        lootlabs_v2: false, // lootlabs v2 Bypass
+        buttonUrl: [''], // Enter Url Want to show Button
+        linkvertise_delay: 0 // Delay Before redirect
+    };
+}
